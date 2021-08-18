efac111dbebcd7ae12294996d30619e9bc56724e
Christian Prince <christianprince132@gmail.com>
2021-08-18 12:37:28 +1000

    Bootloader: read rest of bootloader into memory

--- boot/tools/mkbootsect.c
+++ boot/tools/mkbootsect.c
@@ -3,27 +3,39 @@
 
 int main(int argc, char** argv)
 {
-    assert(argc == 2);
-
     system("dd if=/dev/zero of=boot.img bs=1024 count=10000");
-    system("mkfs.vfat -F16 boot.img");
+    //system("mkfs.vfat -F16 boot.img");
 
-    FILE* in = fopen(argv[1], "r");
+    FILE* bootsect = fopen("bootsect.bin", "r");
     FILE* out = fopen("boot.img", "r+");
+    FILE* loader = fopen("loader.bin", "r");
 
     char jmp[3];
-    fread(jmp, 3, 1, in);
+    fread(jmp, 3, 1, bootsect);
     fwrite(jmp, 3, 1, out);
 
-    fseek(in, 0x3c, SEEK_SET);
+    fseek(bootsect, 0x3c, SEEK_SET);
     fseek(out, 0x3c, SEEK_SET);
 
-    char code[448];
-    fread(code, 448, 1, in);
-    fwrite(code, 448, 1, out);
+    char code[452];
+    fread(code, 452, 1, bootsect);
+    fwrite(code, 452, 1, out);
+
+    printf("%d\n", ftell(out));
+
+    // Write the rest of the bootloader
+    fseek(loader, 0, SEEK_END);
+    size_t loader_size = ftell(loader);
+    fseek(loader, 0, SEEK_SET);
+
+    char* loader_data = malloc(loader_size);
+    fread(loader_data, loader_size, 1, loader);
+
+    printf("%d\n", ftell(bootsect));
+    fwrite(loader_data, loader_size, 1, out);
 
     fclose(out);
-    fclose(in);
-
+    fclose(bootsect);
+    fclose(loader);
     return 0;
 }
